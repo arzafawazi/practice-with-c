ee128806314aab4b03ea8533b889b967e8cf23e0
Arza Fawazi <arza.fawazi103@gmail.com>
2024-08-21 21:33:31 +0200

    first commit

---
+++ ujian/helloword.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Running Hello Word</title>
+</head>
+<body>
+<?php 
+echo "Halo Arza"; ?>
+</body>
+</html>
